2940d36466c8f60ed1bce76ad9cafb0ccc286ced
Amah Nnachi Lofty <loftdot@gmail.com>
2023-08-08 16:11:55 +0100

    5. 00...99

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,3 +1,2 @@
 for i in range(100):
-    print("{:02}".format(i), end=", " if i < 99 else "\n") #The :02 specifies for the value to least two digits, and if it has less than two digits, leading zeros should be added.
-    # If i is not less than 99 (i.e., it's the last value), it adds a newline character \n to start a new line.
+    print("{:02}".format(i), end=", " if i < 99 else "\n")
