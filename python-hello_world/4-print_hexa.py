6feab448741888e9ac8ce3d1a23e0453def5959b
Amah Nnachi Lofty <loftdot@gmail.com>
2023-08-08 15:07:46 +0100

    4. Hexadecimal printing

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -2,4 +2,6 @@
 and prints each number in both decimal and hexadecimal formats. 
 The hex() function is used to convert the decimal number to its hexadecimal representation. The f-string is used for formatting the output."""
 for i in range(99):
-    print(f"{i} = {hex(i)}")
+    print("{} = {}".format(i, hex(i)))
+
+
