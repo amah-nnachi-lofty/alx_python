43b2fd6bd6516489be18cf7e54ac9339f970c7e7
Amah Nnachi Lofty <loftdot@gmail.com>
2023-08-08 14:44:50 +0100

    4. Hexadecimal printing

---
+++ python-hello_world/4-print_hexa.py
@@ -0,0 +1,5 @@
+"""This program uses a for loop to iterate through numbers from 0 to 98 (inclusive),
+and prints each number in both decimal and hexadecimal formats. 
+The hex() function is used to convert the decimal number to its hexadecimal representation. The f-string is used for formatting the output."""
+for i in range(99):
+    print(f"{i} = {hex(i)}")
