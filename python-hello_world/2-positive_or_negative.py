5c8b38e4601db3bdca9b34caf81ff9e3f47acfd7
Amah Nnachi Lofty <loftdot@gmail.com>
2023-08-08 06:15:40 +0100

    2. Positive anything is better than the negative nothing

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print(f"{number} is positive")
+    print(f"{number} is positive") #The f stands for "format" and it tells the Python interpreter to insert the value of the variable number into the string
 elif number < 0:
     print(f"{number} is negative")
 else:
