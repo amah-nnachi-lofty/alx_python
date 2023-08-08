d61fb9827a10ab84690966cf6cbd1d5ebebe437d
Amah Nnachi Lofty <loftdot@gmail.com>
2023-08-08 06:00:29 +0100

    2. Positive anything is better than negative nothing

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -5,14 +5,14 @@ import random
 number = random.randint(-10, 10)
 
 # Check if the number is positive or negative and print the result
-if number == 0:
-    #"The number {} is zero." is a string that contains a placeholder {}. This is where the value of the number variable will be inserted.
+if number > 0:
+    #"The number {} is positive." is a string that contains a placeholder {}. This is where the value of the number variable will be inserted.
     #.format(number) is used to replace the placeholder {} with the value of the number variable. The value of number is dynamically inserted into the string where the {} is located.
-    print("{} is zero".format(number))
-elif number > 0:   #The elif statement in Python stands for "else if." It is used in conditional statements to test multiple conditions in a sequence
-    print("{} is positive".format(number))
-else: 
-    print(" is negative")
+    print(f"{number:d} is positive")
+elif number < 0:   #he elif statement in Python stands for "else if." It is used in conditional statements to test multiple conditions in a sequence
+    print(f"{number:d} is negative")
+else:
+    print(f"{number:d} is zero")
 
-# Print a new line 
+# Print a new line
 print()
