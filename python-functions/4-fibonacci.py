6f55a734933eafc6121b5cc35580a80aa60e28dc
Amah Nnachi Lofty <loftdot@gmail.com>
2023-08-09 13:45:56 +0100

    Write a Python function called fibonacci_sequence that takes a number n as input and returns a list of the first n Fibonacci numbers.

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -4,16 +4,18 @@ def fibonacci_sequence(n):
         return []
 
     # Initialize the sequence with the first two Fibonacci numbers
-    sequence = [0, 1]
+    sequence = [0]
 
     # Generate the Fibonacci sequence up to the nth number
-    for i in range(2, n): # indicates that the loop will start iterating from the index 2, since the initial sequence already contains the first two Fibonacci numbers (0 and 1).
+    if n > 1:
+        sequence.append(1)
+        for i in range(2, n): # indicates that the loop will start iterating from the index 2, since the initial sequence already contains the first two Fibonacci numbers (0 and 1).
         # Calculate the next Fibonacci number by summing the last two numbers
         # sequence[i - 1] refers to the last Fibonacci number that has been generated and appended to the sequence.
         # sequence[i - 2] refers to the second-to-last Fibonacci number that was generated and appended to the sequence.
-        next_fibonacci = sequence[i - 1] + sequence[i - 2]  
-        # Append the next Fibonacci number to the sequence
-        sequence.append(next_fibonacci)
+            next_fibonacci = sequence[i - 1] + sequence[i - 2]  
+            # Append the next Fibonacci number to the sequence
+            sequence.append(next_fibonacci)
 
     # Return the generated Fibonacci sequence
     return sequence
