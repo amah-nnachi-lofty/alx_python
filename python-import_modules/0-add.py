ddd126cefca8f1cb8ba4c2894c7fb0feaacf874e
Amah Nnachi Lofty <loftdot@gmail.com>
2023-08-11 06:47:08 +0100

    Write a program that imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 
-# Import the entire add_0 module and access the add function using dot notation
-import add_0
+# Import the entire module and access the add function using dot notation
+from add import add
 
 def main():
     # Assign values to variables a and b
     a = 1
     b = 2
+
     # Call the add function with variables a and b as arguments
-    result = add_0.add(a, b)
+    result = add(a, b)
+
     # Display the result using string formatting
     print("{} + {} = {}".format(a, b, result))
+
+#  Ensures that the main() function is only executed when the script is run directly
+if __name__ == "__main__":
+    main()
