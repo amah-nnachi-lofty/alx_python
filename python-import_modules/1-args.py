1cb674b6faae13f1ea3a5aca7c03efd0cbc9ae5b
Amah Nnachi Lofty <loftdot@gmail.com>
2023-08-11 20:29:29 +0100

    Write a program that prints the number of and the list of its arguments.

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-import sys
+import sys # built-in function to access the list of command-line arguments passed to the script
 
-def print_arguments(*args):
+def print_arguments(*args): # defines a function named print_arguments that takes a variable number of arguments.
     """Prints the number of and the list of its arguments."""
 
     # Get the number of arguments
@@ -12,15 +12,15 @@ def print_arguments(*args):
         print("0 arguments.")  # Print message for no arguments
     else:
         if num_args == 1:
-            args_str = "argument:"  # Singular form
+            args_str = "argument:"  # Set the string that describes the label for a single argument when printing the output
         else:
-            args_str = "arguments:"  # Plural form
-        print("{} {}".format(num_args, args_str))  # Print count and label
-        for i, arg in enumerate(args, start=1):
-            print("{}: {}".format(i, arg))  # Print argument position and value
+            args_str = "arguments:"  # Set the string that describes the label for a multiple arguments when printing the output
+        print("{} {}".format(num_args, args_str))  # Print count and label dynamically
+        for i, arg in enumerate(args, start=1): # The enumerate function generates pairs of index and value for each element (arg) in the args list
+            print("{}: {}".format(i, arg))  # Print each argument's position (index) and its corresponding value in the output
 
-if __name__ == "__main__":
+if __name__ == "__main__": # Code to be executed only when the script is run directly (as the main program)
     # Test the function with command-line arguments
     args = sys.argv[1:]  # Exclude the script name from arguments
     # Call print_arguments function with command-line arguments
-    print_arguments(*args)
+    print_arguments(*args) # print_arguments function and pass the elements of the args list as individual arguments to the function
