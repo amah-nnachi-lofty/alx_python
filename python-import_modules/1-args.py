e5f614f9a3dde9a869a6b5b90b840caac831159e
Amah Nnachi Lofty <loftdot@gmail.com>
2023-08-11 19:41:33 +0100

    Write a program that prints the number of and the list of its arguments.

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -8,21 +8,19 @@ def print_arguments(*args):
     # Get the number of arguments
     num_args = len(args)
     
-    # If there are no arguments, print a message and return
     if num_args == 0:
         print("0 arguments.")
     else:
         if num_args == 1:
-            args_str = "1 argument:"
+            args_str = "argument:"
         else:
-            args_str = "arguments"
-        print("{}".format(args_str), num_args)
+            args_str = "arguments:"
+        print("{} {}".format(num_args, args_str))
         for i, arg in enumerate(args, start=1):
-            print("{}:".format(i), arg)
+            print("{}: {}".format(i, arg))
 
 if __name__ == "__main__":
     # Test the function with command-line arguments
     args = sys.argv[1:]  # Exclude the script name from arguments
     # Call print_arguments function with command-line arguments
-    # Test the function with various arguments
     print_arguments(*args)
