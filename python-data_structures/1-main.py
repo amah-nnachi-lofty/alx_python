33ee7bd9aac785873af49cd7ce8da57bebb9e71a
Amah Nnachi Lofty <loftdot@gmail.com>
2023-08-12 21:01:43 +0100

    Prototype for: Write a function that prints a matrix of integers.

--- python-data_structures/1-main.py
+++ python-data_structures/1-main.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 
 print_matrix_integer = __import__('1-print_matrix_integer').print_matrix_integer
-
+# Define a matrix with integer values
 matrix = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
 ]
 
+# Call the print_matrix_integer function to print the matrix
 print_matrix_integer(matrix)
-
+# Print a separator line
 print("--")
+# Call the print_matrix_integer function with an empty matrix
 print_matrix_integer()
