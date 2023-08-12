2eb2b120c45aa3ffe8489af7fc98746124f2fdf4
Amah Nnachi Lofty <loftdot@gmail.com>
2023-08-12 06:16:15 +0100

    Write a function that raises a name exception with a message.

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -2,8 +2,12 @@
 
 def raise_exception_msg(message=""):
     """Raises a name exception with a custom message."""
+    
+    # Check if the message is not a string using isinstance()
     if not isinstance(message, str):
+        # If message is not a string, raise a TypeError with an explanatory message
         raise TypeError("Message should be a string")
     
     # Raise a name exception with the provided message
     raise NameError(message)
+
