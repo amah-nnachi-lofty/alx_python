5d76bae855e63e19d3176d6951af4471523b10b8
Amah Nnachi Lofty <loftdot@gmail.com>
2023-08-16 00:45:39 +0100

    Write a class BaseGeometry (based on 4-base_geometry.py).
        Public instance method: def area(self): that raises an Exception with the message area() is not implemented

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -27,9 +27,11 @@ class BaseGeometry(metaclass=BaseMetaClass):
         """
         return [attr for attr in super().__dir__() if attr != '__init_subclass__']
 
-    class BaseGeometry:
-        def area(self):
-            raise Exception("area() is not implemented")
+    def area(self):
+        """
+        Public instance method that raises an Exception.
+        """
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """
@@ -41,7 +43,7 @@ class BaseGeometry(metaclass=BaseMetaClass):
             TypeError: If the value is not an integer.
             ValueError: If the value is less than or equal to 0.
         """
-        if not type(value) is int:
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
