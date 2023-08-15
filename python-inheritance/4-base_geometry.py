f769c6575aad5999d59fdbaa081d6af34b2368e1
Amah Nnachi Lofty <loftdot@gmail.com>
2023-08-16 00:32:34 +0100

    Write a class BaseGeometry (based on 3-base_geometry.py).

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+This module defines the BaseGeometry class and a metaclass BaseMetaClass.
+"""
+
+class BaseMetaClass(type):
+    """
+    Metaclass that customizes the behavior of the dir() function.
+    It excludes the '__init_subclass__' attribute from the result.
+    """
+    def __dir__(cls):
+        """
+        Return a list of attributes of the class, excluding '__init_subclass__'.
+        """
+        return [attr for attr in super().__dir__() if attr != '__init_subclass__']
+
+class BaseGeometry(metaclass=BaseMetaClass):
+    """
+    Base class for geometry-related classes, using BaseMetaClass metaclass.
+    It excludes the '__init_subclass__' attribute from the result of dir().
+    """
+    def __dir__(cls):
+        """
+        Return a list of attributes of the class, excluding '__init_subclass__'.
+        """
+        return [attr for attr in super().__dir__() if attr != '__init_subclass__']
+
+    def area(self):
+        """
+        Public instance method that raises an Exception.
+        """
+        raise Exception("area() is not implemented")
