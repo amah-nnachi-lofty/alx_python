f9c85c713d3e7bce1b7e91ed8bff699fa70e65ea
Amah Nnachi Lofty <loftdot@gmail.com>
2023-08-16 00:10:07 +0100

    Write an empty class BaseGeometry.

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,13 +1,7 @@
-#!/usr/bin/python3
-""" This module defines the BaseGeometry class. """ 
-class BaseGeometry: 
-    """ Empty class representing a basic geometry. """
-    pass
 """
 Module: 3-base_geometry
 Empty class for BaseGeometry
 """
-
 
 class BaseGeometry:
     """
