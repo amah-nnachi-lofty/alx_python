cf9ce75dca4aa88133bebda0b043a7ed3ff019e0
Amah Nnachi Lofty <loftdot@gmail.com>
2023-08-15 12:43:38 +0100

    Write a class Square that defines a square by:
        Private instance attribute: size
        Instantiation with size (no type/value verification)

---
+++ python-classes/0-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a Square instance with the given size.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
