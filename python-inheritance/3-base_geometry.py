d91d98e64368c64ed5dc35749717edf62e63a66f
Amah Nnachi Lofty <loftdot@gmail.com>
2023-08-16 00:20:12 +0100

    Write an empty class BaseGeometry.

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,10 +1,55 @@
-#!/usr/bin/python3
-"""
-Module for BaseGeometry class.
+"""Module for BaseGeometry class.
+
+This module defines the BaseGeometry class and its metaclass.
+
+The BaseGeometry class is an abstract base class for geometric objects.
+It cannot be instantiated directly.
 """
 
-class BaseGeometry:
+
+class BaseMetaClass(type):
+    """Metaclass for BaseGeometry class.
+
+    This metaclass defines a custom `__dir__` method for the BaseGeometry class
+    that returns all attributes of the class except `__init_subclass__`.
+
+    Returns:
+        A list of attributes of the class.
     """
-    An empty class representing a base geometry.
+
+    def __dir__(cls):
+        """Return all attributes of the class except `__init_subclass__`.
+
+        Args:
+            cls: The class to get the attributes for.
+
+        Returns:
+            A list of attributes of the class.
+        """
+        return [attribute for attribute in super().__dir__(cls) if attribute != '__init_subclass__']
+
+
+class BaseGeometry(metaclass=BaseMetaClass):
+    """Abstract base class for geometric objects.
+
+    Args:
+        metaclass: The metaclass for the class.
+
+    Raises:
+        TypeError: If the class is instantiated.
     """
-    pass
+
+    def __init__(self):
+        raise TypeError("Cannot instantiate abstract base class.")
+
+    def __dir__(cls):
+        """Return all attributes of the class except `__init_subclass__`.
+
+        Args:
+            cls: The class to get the attributes for.
+
+        Returns:
+            A list of attributes of the class.
+        """
+        return [attribute for attribute in super().__dir__(cls) if attribute != '__init_subclass__']
+
