741c25785cefb034da6cb0d0a3034bc3a888bf3e
Amah Nnachi Lofty <loftdot@gmail.com>
2023-08-16 00:25:09 +0100

    Write an empty class BaseGeometry.

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,55 +1,27 @@
-"""Module for BaseGeometry class.
+#!/usr/bin/python3
 
-This module defines the BaseGeometry class and its metaclass.
-
-The BaseGeometry class is an abstract base class for geometric objects.
-It cannot be instantiated directly.
+"""
+This module defines the BaseGeometry class and a metaclass BaseMetaClass.
 """
 
-
 class BaseMetaClass(type):
-    """Metaclass for BaseGeometry class.
-
-    This metaclass defines a custom `__dir__` method for the BaseGeometry class
-    that returns all attributes of the class except `__init_subclass__`.
-
-    Returns:
-        A list of attributes of the class.
     """
-
+    Metaclass that customizes the behavior of the dir() function.
+    It excludes the '__init_subclass__' attribute from the result.
+    """
     def __dir__(cls):
-        """Return all attributes of the class except `__init_subclass__`.
-
-        Args:
-            cls: The class to get the attributes for.
-
-        Returns:
-            A list of attributes of the class.
         """
-        return [attribute for attribute in super().__dir__(cls) if attribute != '__init_subclass__']
-
+        Return a list of attributes of the class, excluding '__init_subclass__'.
+        """
+        return [attr for attr in super().__dir__() if attr != '__init_subclass__']
 
 class BaseGeometry(metaclass=BaseMetaClass):
-    """Abstract base class for geometric objects.
-
-    Args:
-        metaclass: The metaclass for the class.
-
-    Raises:
-        TypeError: If the class is instantiated.
     """
-
-    def __init__(self):
-        raise TypeError("Cannot instantiate abstract base class.")
-
+    Base class for geometry-related classes, using BaseMetaClass metaclass.
+    It excludes the '__init_subclass__' attribute from the result of dir().
+    """
     def __dir__(cls):
-        """Return all attributes of the class except `__init_subclass__`.
-
-        Args:
-            cls: The class to get the attributes for.
-
-        Returns:
-            A list of attributes of the class.
         """
-        return [attribute for attribute in super().__dir__(cls) if attribute != '__init_subclass__']
-
+        Return a list of attributes of the class, excluding '__init_subclass__'.
+        """
+        return [attr for attr in super().__dir__() if attr != '__init_subclass__']
