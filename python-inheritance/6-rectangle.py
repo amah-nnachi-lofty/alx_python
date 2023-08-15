ff82f66cf2a401f2db32c98699f936fc98619429
Amah Nnachi Lofty <loftdot@gmail.com>
2023-08-16 00:59:25 +0100

    Write a class Rectangle that inherits from BaseGeometry (5-base_geometry.py).
        Instantiation with width and height: def __init__(self, width, height):

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -35,5 +35,3 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-print(issubclass(Rectangle, BaseGeometry))  # Output: True
