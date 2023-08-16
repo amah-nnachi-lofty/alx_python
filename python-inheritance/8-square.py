14048b48a2029a2c788a388dac0402b9b02a55be
Amah Nnachi Lofty <loftdot@gmail.com>
2023-08-16 02:18:57 +0100

    Write a class Square that inherits from Rectangle (7-rectangle.py):

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -4,6 +4,17 @@
 This module defines the BaseGeometry class and a metaclass BaseMetaClass.
 """
 
+class BaseMetaClass(type):
+    """
+    Metaclass that customizes the behavior of the dir() function.
+    It excludes the '__init_subclass__' attribute from the result.
+    """
+    def __dir__(cls):
+        """
+        Return a list of attributes of the class, excluding '__init_subclass__'.
+        """
+        return [attr for attr in super().__dir__() if attr != '__init_subclass__']
+
 class BaseGeometry(metaclass=BaseMetaClass):
     """
     Base class for geometry-related classes, using BaseMetaClass metaclass.
