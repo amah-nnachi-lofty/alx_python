cfd22bb4adf647000bb3bbf601868153aedfb725
Amah Nnachi Lofty <loftdot@gmail.com>
2023-08-22 18:22:19 +0100

    Create a folder named models with an empty file __init__.py inside - with this file, the folder will become a Python package

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+"""
+This module defines the Base class.
+
+The Base class has a private class attribute `__nb_objects` and a public instance attribute `id`.
+
+The `__nb_objects` attribute is incremented by 1 each time a new object of the Base class is created.
+
+The `id` attribute is assigned the value of the `id` parameter passed to the constructor, or a unique id if no value is passed.
+"""
+
+
+class Base:
+  """
+  This class represents a base class.
+
+  Args:
+    id (int, optional): The id of the object. Defaults to None.
+  """
+
+  __nb_objects = 0
+
+  def __init__(self, id=None):
+    """
+    Constructor of the Base class.
+
+    Args:
+      id (int, optional): The id of the object. Defaults to None.
+    """
+
+    """ Check if the id parameter is not None """
+    if id is not None:
+      """ Assign the value of the id parameter to the id attribute """
+      self.id = id
+    else:
+      """ Increment the value of the __nb_objects attribute by 1 """
+      self.id = Base.__nb_objects
+      """ Assign the new value of the __nb_objects attribute to the id attribute """
+      Base.__nb_objects += 1
+
