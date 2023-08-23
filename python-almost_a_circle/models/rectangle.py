9d9679b673ad9ff701a4059548dca9958c2bad99
Amah Nnachi Lofty <loftdot@gmail.com>
2023-08-23 07:14:29 +0100

    Update the class Rectangle by adding the public method def update(self, *args): that assigns an argument to each attribute:
        1st argument should be the id attribute
        2nd argument should be the width attribute
        3rd argument should be the height attribute
        4th argument should be the x attribute
        5th argument should be the y attribute

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -33,73 +33,20 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
-    @property
-    def width(self):
-        """Getter method for width attribute."""
-        return self.__width
+    # Other methods...
 
-    @width.setter
-    def width(self, value):
-        """Setter method for width attribute."""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = value
+    def update(self, *args):
+        """
+        Updates the attributes of the rectangle instance with the provided arguments.
 
-    @property
-    def height(self):
-        """Getter method for height attribute."""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """Setter method for height attribute."""
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
-
-    @property
-    def x(self):
-        """Getter method for x attribute."""
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        """Setter method for x attribute."""
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = value
-
-    @property
-    def y(self):
-        """Getter method for y attribute."""
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        """Setter method for y attribute."""
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
-        self.__y = value
-
-    def area(self):
-        """Calculates and returns the area of the rectangle."""
-        return self.__width * self.__height
-
-    def display(self):
-        """Displays a visual representation of the rectangle using '#' characters."""
-        for _ in range(self.__y):
-            print()
-        for _ in range(self.__height):
-            print(" " * self.__x + "#" * self.__width)
+        Args:
+            *args: The arguments in the order: id, width, height, x, y.
+        """
+        if args:
+            attributes = ['id', 'width', 'height', 'x', 'y']
+            for attr, value in zip(attributes, args):
+                setattr(self, attr, value)
 
     def __str__(self):
         """Returns a custom string representation of the rectangle."""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
