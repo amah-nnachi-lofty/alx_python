b9cdb5f9fd544c857538d44caa68a5ab7fbb6261
Amah Nnachi Lofty <loftdot@gmail.com>
2023-08-23 12:14:59 +0100

    Square class, which is a subclass of the Rectangle class. The Square class represents a square shape and inherits attributes and methods from the Rectangle class. By inheriting from the Rectangle class, the Square class leverages its attributes and methods, while also providing specific functionality for squares. This approach demonstrates the concept of object-oriented programming, where common features are shared among related classes while allowing for specialization and extension

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -33,7 +33,7 @@ class Square(Rectangle):
             ValueError: If size, x, or y are not greater than or equal to 0.
         """
         if not isinstance(size, int):
-            raise TypeError("size must be an integer >= 0")
+            raise TypeError("size must be an integer")
         if size <= 0:
             raise ValueError("size must be > 0")
         if not isinstance(x, int):
