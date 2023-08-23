d28c0485219878f8fc24fe8ad4ff64f90b60a2f0
Amah Nnachi Lofty <loftdot@gmail.com>
2023-08-23 16:09:26 +0100

    Write the class Rectangle that inherits from Base

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -29,26 +29,64 @@ class Rectangle(Base):
             y (int, optional): The y-coordinate of the top-left corner (default: 0).
             id (int, optional): A unique identifier for the instance (default: generated).
         """
-        super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        super().__init__(id)  # Call the base class's constructor with the provided id
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
-    # Other methods...
+    @property
+    def width(self):
+        """Getter method for width attribute."""
+        return self.__width
 
-    def update(self, *args):
-        """
-        Updates the attributes of the rectangle instance with the provided arguments.
+    @width.setter
+    def width(self, value):
+        """Setter method for width attribute."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
 
-        Args:
-            *args: The arguments in the order: id, width, height, x, y.
-        """
-        if args:
-            attributes = ['id', 'width', 'height', 'x', 'y']
-            for attr, value in zip(attributes, args):
-                setattr(self, attr, value)
+    @property
+    def height(self):
+        """Getter method for height attribute."""
+        return self.__height
 
-    def __str__(self):
-        """Returns a custom string representation of the rectangle."""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
+    @height.setter
+    def height(self, value):
+        """Setter method for height attribute."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter method for x attribute."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setter method for x attribute."""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter method for y attribute."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setter method for y attribute."""
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
