e2bcae9123ddb334e85c9dc3de25a622e1d67cf5
Amah Nnachi Lofty <loftdot@gmail.com>
2023-08-23 17:40:39 +0100

    Update the class Rectangle by updating the public method def update(self, *args): by changing the prototype to update(self, *args, **kwargs) that assigns a key/value argument to attributes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -68,19 +68,3 @@ class Rectangle(Base):
     def __str__(self):
         """Returns a custom string representation of the rectangle."""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
-
-if __name__ == "__main__":
-    r1 = Rectangle(10, 10, 10, 10)
-    print(r1)
-
-    r1.update(height=1)
-    print(r1)
-
-    r1.update(width=1, x=2)
-    print(r1)
-
-    r1.update(y=1, width=2, x=3, id=89)
-    print(r1)
-
-    r1.update(x=1, height=2, y=3, width=4)
-    print(r1)
