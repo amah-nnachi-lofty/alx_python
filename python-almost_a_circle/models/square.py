dc2d321fa3bba39b9aea217a557493297314b478
Amah Nnachi Lofty <loftdot@gmail.com>
2023-08-23 08:56:44 +0100

    Square class, which is a subclass of the Rectangle class. The Square class represents a square shape and inherits attributes and methods from the Rectangle class. By inheriting from the Rectangle class, the Square class leverages its attributes and methods, while also providing specific functionality for squares. This approach demonstrates the concept of object-oriented programming, where common features are shared among related classes while allowing for specialization and extension.

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """
+    The Square class represents a square shape.
+
+    Attributes:
+        size (int): The size of the square's sides.
+        x (int): The x-coordinate of the top-left corner of the square.
+        y (int): The y-coordinate of the top-left corner of the square.
+        id (int): A unique identifier for the square instance.
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square's sides.
+            x (int, optional): The x-coordinate of the top-left corner (default: 0).
+            y (int, optional): The y-coordinate of the top-left corner (default: 0).
+            id (int, optional): A unique identifier for the instance (default: generated).
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        Returns a custom string representation of the square.
+
+        Returns:
+            str: A formatted string representation of the square.
+        """
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+
+    def area(self):
+        """
+        Calculates and returns the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.width * self.width
+
+    def display(self):
+        """
+        Displays the square using '#' characters.
+
+        Prints the square's representation with appropriate adjustments for coordinates.
+        """
+        for _ in range(self.y):
+            print()
+        for _ in range(self.height):
+            print(" " * self.x + "#" * self.width)
