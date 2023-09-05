1745dfaf62610eaf36ac6f6d0c0ea8b8442473fb
Amah Nnachi Lofty <loftdot@gmail.com>
2023-09-05 11:00:15 +0100

    Write the class Square that inherits from Rectangle

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,29 +9,46 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """
-    A class Square inherits from class Rectangle
+    The Square class represents a square shape.
+
+    Attributes:
+        size (int): The size of the square's sides.
+        x (int): The x-coordinate of the top-left corner of the square.
+        y (int): The y-coordinate of the top-left corner of the square.
+        id (int): A unique identifier for the square instance.
     """
 
     def __init__(self, size, x=0, y=0, id=None):
         """
-        Initialize a new Square instance
+        Initializes a new instance of the Square class.
 
         Args:
-            size (int): The size of the square's sides
-            x (int): The x coordinate of the top left corner
-            y (int): The y coordinate of the top left corner
-            id (int): The id of the square
+            size (int): The size of the square's sides.
+            x (int, optional): The x-coordinate of the top-left corner (default: 0).
+            y (int, optional): The y-coordinate of the top-left corner (default: 0).
+            id (int, optional): A unique identifier for the instance (default: generated).
+
+        Raises:
+            TypeError: If size, x, or y are not integers.
+            ValueError: If size, x, or y are not greater than or equal to 0.
         """
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """Getter method for size."""
+        """
+        Calculates and returns the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
         return self.width
 
     @size.setter
     def size(self, value):
-        """Setter method for wsize."""
+        """
+        Setter method for wsize
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -42,9 +59,9 @@ class Square(Rectangle):
 
     def __str__(self):
         """
-        Returns the string representation of the Square instance
+        Returns a custom string representation of the square.
 
         Returns:
-            str: [Square] (<id>) <x>/<y> - <size>
+            str: A formatted string representation of the square.
         """
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size)
