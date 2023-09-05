b3360eca20a614218eedc31ad3d5e305241a2a1d
Amah Nnachi Lofty <loftdot@gmail.com>
2023-09-05 10:49:40 +0100

    Write the class Square that inherits from Rectangle

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,95 +9,42 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """
-    The Square class represents a square shape.
-
-    Attributes:
-        size (int): The size of the square's sides.
-        x (int): The x-coordinate of the top-left corner of the square.
-        y (int): The y-coordinate of the top-left corner of the square.
-        id (int): A unique identifier for the square instance.
+    A class Square inherits from class Rectangle
     """
 
     def __init__(self, size, x=0, y=0, id=None):
         """
-        Initializes a new instance of the Square class.
+        Initialize a new Square instance
 
         Args:
-            size (int): The size of the square's sides.
-            x (int, optional): The x-coordinate of the top-left corner (default: 0).
-            y (int, optional): The y-coordinate of the top-left corner (default: 0).
-            id (int, optional): A unique identifier for the instance (default: generated).
-
-        Raises:
-            TypeError: If size, x, or y are not integers.
-            ValueError: If size, x, or y are not greater than or equal to 0.
+            size (int): The size of the square's sides
+            x (int): The x coordinate of the top left corner
+            y (int): The y coordinate of the top left corner
+            id (int): The id of the square
         """
-        if not isinstance(size, int) or size < 0:
-            raise TypeError("size must be an integer >= 0")
-        if size <= 0:
-            raise ValueError("size must be > 0")
-        if not isinstance(x, int):
-            raise TypeError("x must be an integer")
-        if x < 0:
-            raise ValueError("x must be >= 0")
-        if not isinstance(y, int):
-            raise TypeError("y must be an integer")
-        if y < 0:
-            raise ValueError("y must be >= 0")
-
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """Getter for the size attribute."""
-        return self.width  # Since size and width are equal for a square
+        """Getter method for size."""
+        return self.width
 
     @size.setter
     def size(self, value):
-        """Setter for the size attribute."""
-        if not isinstance(value, int) or value <= 0:
+        """Setter method for wsize."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
             raise ValueError("width must be > 0")
-        self.width = self.height = value  # Set both width and height to the same value
+
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """
-        Returns a custom string representation of the square.
+        Returns the string representation of the Square instance
 
         Returns:
-            str: A formatted string representation of the square.
+            str: [Square] (<id>) <x>/<y> - <size>
         """
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
-
-    def update(self, *args, **kwargs):
-        """
-        Updates the attributes of the square instance with the provided arguments.
-
-        Args:
-            *args: The positional arguments in the order: id, size, x, y.
-            **kwargs: The keyword arguments to update specific attributes.
-        """
-        super().update(*args, **kwargs)
-
-        # The `size` attribute takes precedence over the `width` attribute.
-        if "size" in kwargs and isinstance(kwargs["size"], int):
-            self.width = self.height = kwargs["size"]
-
-    def area(self):
-        """
-        Calculates and returns the area of the square.
-
-        Returns:
-            int: The area of the square.
-        """
-        return self.width * self.width
-
-    def display(self):
-        """
-        Displays the square using '#' characters.
-
-        Prints the square's representation with appropriate adjustments for coordinates.
-        """
-        for _ in range(self.y):
-            print()
-        for _ in range(self.height):
-            print(" " * self.x + "#" * self.width)
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size)
