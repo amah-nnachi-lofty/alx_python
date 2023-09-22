0c1a74fb28ff60c6fa9c4c4eeeaf8cc945cf7e37
Amah Nnachi Lofty <loftdot@gmail.com>
2023-09-22 18:15:54 +0100

    Write a Python script that takes in a URL, sends a request to the URL and displays the value of the variable X-Request-Id in the response header

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,34 @@
+"""
+Write a Python script that takes in a URL, sends a request to the URL and displays the value of the variable X-Request-Id in the response header
+"""
+
+import requests
+import sys
+
+def get_request_id(url):
+  """Sends a request to the specified URL and returns the value of the `X-Request-Id` header in the response.
+
+  Args:
+    url: The URL to send a request to.
+
+  Returns:
+    The value of the `X-Request-Id` header in the response, or `None` if the header is not present.
+  """
+
+  response = requests.get(url)
+  x_request_id = response.headers.get('X-Request-Id')
+  return x_request_id
+
+def main():
+  """Takes in a URL from the user, sends a request to the URL, and displays the value of the `X-Request-Id` header in the response."""
+
+  url = input('Enter a URL: ')
+  x_request_id = get_request_id(url)
+
+  if x_request_id is not None:
+    print('The value of the `X-Request-Id` header in the response is: {}'.format(x_request_id))
+  else:
+    print('The `X-Request-Id` header is not present in the response.')
+
+if __name__ == '__main__':
+  main()
