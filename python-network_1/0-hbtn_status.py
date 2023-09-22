e08c345b4cd3d2314607d46364dc3e5f19d14e2d
Amah Nnachi Lofty <loftdot@gmail.com>
2023-09-22 17:49:04 +0100

    0. What's my status? #1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,38 +1,56 @@
-"""
-Fetches the status of the alu-intranet.hbtn.io service and displays it in a tabulated format.
-
-Returns:
-    None
-"""
-
 import requests
 
-
-def fetch_status():
-    """Fetches the status of the alu-intranet.hbtn.io service.
-
-    Returns:
-        A JSON object containing the status of the service.
+def fetch_status(url):
     """
-
-    response = requests.get('https://alu-intranet.hbtn.io/status')
-    if response.status_code != 200:
-        raise Exception('Failed to fetch status: {}'.format(response.status_code))
-    return response.json()
-
-
-def display_status(status):
-    """Displays the status of the alu-intranet.hbtn.io service in a tabulated format.
+    Fetches status information from a given URL using the requests package.
 
     Args:
-        status: A JSON object containing the status of the service.
+        url (str): The URL to fetch status information from.
+
+    Returns:
+        dict: A dictionary containing the status information, or None on failure.
+
+    Example:
+        status_data = fetch_status("https://alu-intranet.hbtn.io/status")
+        if status_data:
+            print_status(status_data)
     """
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception if the request was not successful
 
-    print('Status:')
-    for key, value in status.items():
-        print('\t- {}: {}'.format(key, value))
+        if response.headers.get('content-type') == 'application/json':
+            data = response.json()
+            return data
+        else:
+            print("Received a non-JSON response.")
+            return None
+    except requests.exceptions.RequestException as e:
+        print(f"Request Exception: {e}")
+        return None
+    except ValueError as e:
+        print(f"ValueError: {e}")
+        return None
 
+def print_status(status_data):
+    """
+    Displays the status information with tabulation.
 
-if __name__ == '__main__':
-    status = fetch_status()
-    display_status(status)
+    Args:
+        status_data (dict): A dictionary containing the status information.
+
+    Example:
+        print_status(status_data)
+    """
+    if status_data is not None:
+        print("Body response:")
+        print("\t- type:", type(status_data))
+        print("\t- content:", status_data)
+    else:
+        print("Failed to fetch data.")
+
+if __name__ == "__main__":
+    url = "https://alu-intranet.hbtn.io/status"
+    status_data = fetch_status(url)
+    if status_data:
+        print_status(status_data)
