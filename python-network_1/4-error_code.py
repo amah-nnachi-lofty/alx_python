40b10257023d0d6e1d421b1460ec7ddf93d27290
Amah Nnachi Lofty <loftdot@gmail.com>
2023-09-23 12:06:55 +0100

    Write a Python script that takes in a URL, sends a request to the URL and displays the body of the response

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -8,24 +8,45 @@ If the HTTP status code is greater than or equal to 400, print: Error code: foll
 import requests
 import sys
 
-def get_request(url):
-    """
-    Sends a GET request to the given URL.
+def send_request(url):
+  """Sends a request to the specified URL and returns the response.
 
-    Args:
-        url: The URL to send a GET request to.
+  Args:
+    url: The URL to send a request to.
 
-    Returns:
-        The response text from the server.
-    """
+  Returns:
+    A `Response` object containing the response from the server.
+  """
 
-    response = requests.get(url)
-    if response.status_code >= 400:
-        print("Error code:", response.status_code)
-    return response.text
+  response = requests.get(url)
+  return response
 
+def display_response(response):
+  """Displays the body of the response.
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    response_text = get_request(url)
-    print(response_text)
+  Args:
+    response: A `Response` object containing the response from the server.
+  """
+
+  print(response.content)
+
+def main():
+  """Takes in a URL from the user, sends a request to the URL, and displays the body of the response."""
+
+  url = input('Enter a URL: ')
+
+  """ Send a request to the URL"""
+  response = send_request(url)
+
+  """ Check the HTTP status code"""
+  status_code = response.status_code
+
+  """ If the HTTP status code is greater than or equal to 400, print an error message"""
+  if status_code >= 400:
+    print('Error code: {}'.format(status_code))
+  else:
+    """ Display the body of the response """
+    display_response(response)
+
+if __name__ == '__main__':
+  main()
