66b98778114395f749af2a523b3ac7e0209b3bec
amah-nnachi-lofty <loftdot@gmail.com>
2023-10-01 12:04:38 -0700

    Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,33 +1,33 @@
 #!/usr/bin/python3
 """
-Displays all values in the states table of hbtn_0e_0_usa where name matches the provided argument.
-
-Usage: ./2-my_filter_states.py <mysql_username> <mysql_password> <database_name> <state_name>
+Displays all values in the states table of hbtn_0e_0_usa
+where name matches the provided argument.
 """
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Extract command-line arguments
-    username, password, database, state_name = sys.argv[1:]
-
     # Establish a database connection
-    conn = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-    # Create a cursor object to interact with the database
-    cursor = conn.cursor()
+    # Create a cursor object to execute SQL queries
+    cursor = db.cursor()
 
-    # Execute SQL query to select states with the specified name
+    # SQL query to filter states by the provided name
     query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    state_name = sys.argv[4]
+
+    # Execute the query with the provided state name
     cursor.execute(query, (state_name,))
     states = cursor.fetchall()
 
-    # Print states that match the criteria
+    # Display results as specified
     for state in states:
         print(state)
 
     # Close the cursor and database connection
     cursor.close()
-    conn.close()
+    db.close()
 
