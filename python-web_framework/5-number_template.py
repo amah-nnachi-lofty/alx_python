1b92d920a375fc022faf570af6887f6e1f74f0e0
Amah Nnachi Lofty <loftdot@gmail.com>
2023-10-05 00:55:30 +0100

    Starts a Flask web application with specific routes, including dynamic routes, default values, and number_template and number_odd_or_even routes

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
 Starts a Flask web application with specific routes, including dynamic routes,
-default values, and number_template route.
+default values, and number_template and number_odd_or_even routes.
 """
 
 from flask import Flask, render_template
-from markupsafe import escape
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
@@ -16,6 +16,7 @@ def hello_hbnb():
     """
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """
@@ -23,39 +24,48 @@ def hbnb():
     """
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
-def c(text):
+def c_text(text):
     """
     Displays "C ", followed by the value of the text variable.
     Replaces underscore _ symbols with a space.
     """
-    formatted_text = escape(text.replace('_', ' '))
-    return "C {}".format(formatted_text)
+    return "C {}".format(text.replace("_", " "))
+
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python(text):
+def python_text(text):
     """
     Displays "Python ", followed by the value of the text variable.
     Replaces underscore _ symbols with a space.
     """
-    formatted_text = escape(text.replace('_', ' '))
-    return "Python {}".format(formatted_text)
+    return "Python {}".format(text.replace("_", " "))
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
+
+@app.route('/number/<n>', strict_slashes=False)
+def number_n(n):
     """
     Displays "{} is a number" only if n is an integer.
+    Returns 404 if n is not a valid integer.
     """
-    return "{} is a number".format(n)
+    try:
+        n_int = int(n)
+        return "{} is a number".format(n_int)
+    except ValueError:
+        return "404 Not Found", 404
+
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
+def number_template_n(n):
     """
-    Renders '5-number.html' template with the provided number and whether it's even or odd.
+    Renders '6-number_template.html' template with the provided number.
+    Displays if the number is even or odd.
     """
     is_even = n % 2 == 0
-    return render_template('5-number.html', number=n, is_even=is_even)
+    return render_template('6-number_odd_or_even.html', number=n, is_even=is_even)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
