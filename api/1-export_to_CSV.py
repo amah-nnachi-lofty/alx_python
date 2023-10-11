ddbdf50e037468a604fbf7178d9ae3260c528656
amah-nnachi-lofty <loftdot@gmail.com>
2023-10-11 15:02:38 -0700

    Export to CSV : Using what you did in the task #0, extend your Python script to export data in the CSV format.

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -10,44 +10,52 @@ Usage:
 import csv
 import requests
 import sys
-import os
-
-def user_info(employee_id):
-    """
-    Fetches employee's completed tasks and exports the data to CSV.
-
-    Args:
-        employee_id (str): Employee ID.
-    """
-    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-    response = requests.get(url)
-    todos = response.json()
-
-    if not todos:
-        print(f"No data available for employee with ID {employee_id}")
-        return
-    
-    # Ensure the CSV file exists or create a new one
-    csv_filename = f'{employee_id}.csv'
-    header = ['Task ID', 'Employee ID', 'Employee Name', 'Completed', 'Task Title']
-    file_exists = os.path.exists(csv_filename)
-    with open(csv_filename, 'a', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile)
-        # Write header only if the file is newly created
-        if not file_exists:
-            csv_writer.writerow(header)
-
-        # Write tasks to the CSV file
-        for todo in todos:
-            csv_writer.writerow([todo.get('id'), employee_id, todo.get("userId"), todo.get("completed"), todo.get("title")])
-
-    print(f"Data exported to {csv_filename}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-    else:
-        employee_id = sys.argv[1]
-        user_info(employee_id)
 
+    base_url = "https://jsonplaceholder.typicode.com"
+    employee_id = sys.argv[1]
 
+    # Details of the employer
+    employee_url = "{}/users/{}".format(base_url, employee_id)
+    employee_response = requests.get(employee_url)
+    employee_data = employee_response.json()
+
+    if 'name' not in employee_data:
+        print("Employee not found.")
+        sys.exit(1)
+
+    employee_username = employee_data.get('username')
+
+    # Employee TODO list and status of progress
+    todo_url = "{}/users/{}/todos".format(base_url, employee_id)
+    todo_response = requests.get(todo_url)
+    todo_data = todo_response.json()
+
+    # computation of tasks
+    total_tasks = len(todo_data)
+    completed_tasks = sum(1 for task in todo_data if task.get("completed"))
+
+    # Print taks level
+    print("Employee {} is done with tasks({}/{}):".format(employee_username,
+                                                          completed_tasks, total_tasks))
+
+    # show completed task titles
+    for task in todo_data:
+        if task.get("completed"):
+            formatted_task_title = "\t {}".format(task.get("title"))
+            print(formatted_task_title)
+
+    # Write tasks to the CSV file
+    file_name = '{}.csv'.format(employee_id)
+    with open(file_name, mode='w', newline='',) as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',',
+                                quotechar='"', quoting=csv.QUOTE_ALL)
+
+        # Ensure the CSV file exists or create a new one
+        #  header = ['Task ID', 'Employee ID', 'Employee Name', 'Completed', 'Task Title']
+
+        # writing to each rows of the csv file 
+        for task in todo_data:
+            csv_writer.writerow([employee_id, employee_username,
+                                task['completed'], task['title']])
