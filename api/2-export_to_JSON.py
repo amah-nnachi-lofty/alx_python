e0be63344119fc46e5a6b651eb1f64f192f61fb2
amah-nnachi-lofty <loftdot@gmail.com>
2023-10-23 13:56:08 -0700

    Export to json

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -24,46 +24,36 @@ import json
 import requests
 import sys
 
-# Check if the correct number of command line arguments is provided
+
 if len(sys.argv) != 2:
     print("Usage: python3 2-export_to_JSON.py <employee_id>")
     sys.exit(1)
-    
-# Extract employee ID from command line arguments
+
 employee_id = sys.argv[1]
+url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+response = requests.get(url)
+employee_name = response.json().get("username")
 
-# Make a request to the API endpoint to get employee details
-url_user = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-response_user = requests.get(url_user)
-user_data = response_user.json()
-
-# Check if the employee with the given ID exists
-if not user_data:
+if not employee_name:
     print(f"No employee found with ID {employee_id}")
     sys.exit(1)
 
-# Make a request to the API endpoint to get employee's TODO list
-url_todos = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-response_todos = requests.get(url_todos)
-todos = response_todos.json()
-
-# Create a list of TODO items with the specified format
-todo_list = []
+url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+response = requests.get(url)
+todos = response.json()
+employee_tasks = []
 for todo in todos:
     task_data = {
         "task": todo.get("title"),
         "completed": todo.get("completed"),
-        "username": user_data.get("username")
+        "username": employee_name
     }
-    todo_list.append(task_data)
+    employee_tasks.append(task_data)
 
-# Create the output dictionary with user ID as the key and a list of TODO items as the value
-output_data = {employee_id: todo_list}
 
-# Export data to JSON file
-output_file_name = f"{employee_id}.json"
-with open(output_file_name, "w") as json_file:
-    json.dump(output_data, json_file, indent=4)
+output_file = f"{employee_id}.json"
 
-print(f"Data exported to {output_file_name} successfully.")
+with open(output_file, 'w') as json_file:
+    json.dump({employee_id: employee_tasks}, json_file, indent=4)
 
+print(f"Data exported to {output_file}")
