bfe2158e4182551583f8835ff542f4ab2143f934
Amah Nnachi Lofty <loftdot@gmail.com>
2023-10-23 20:15:10 +0100

    Expoert to CSV: Using what you did in the task #0, extend your Python script to export data in the CSV format

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 """
-Script to export data from an API about an employee's TODO list progress in CSV format.
+Script to gather data from an API about an employee's TODO list progress.
 
 This script uses the requests module to make requests to the API endpoints for
 getting employee details and TODO list items. It then calculates the employee's
-TODO list progress and exports it to a CSV file in the specified format.
+TODO list progress and prints it to the standard output in the following format:
+"USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
 
-Format must be: 
-"USER_ID","EMPLOYEE_NAME","TASK_TITLE","TASK_COMPLETED_STATUS"
+where:
+
+* USER_ID: ID of the employee
+* USERNAME: username of the employee
+* TASK_COMPLETED_STATUS: True if the task is completed, False otherwise
+* TASK_TITLE: title of the task
 
 Args:
   employee_id: The integer employee ID.
@@ -16,9 +21,9 @@ Returns:
   None
 """
 
-import csv
 import requests
 import sys
+import csv
 
 def export_to_CSV(employee_id, todos):
     """
@@ -30,18 +35,18 @@ def export_to_CSV(employee_id, todos):
     """
     with open(f"{employee_id}.csv", "w", newline="", encoding="UTF-8") as csv_file:
         csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-        csv_writer.writerow(["USER_ID", "EMPLOYEE_NAME", "TASK_TITLE", "TASK_COMPLETED_STATUS"])
+        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
 
-        employee_name = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}").json().get("name")
+        employee_name = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}").json().get("username")
 
         for todo in todos:
             task_title = todo.get("title")
             completed_status = str(todo.get("completed"))
-            csv_writer.writerow([employee_id, employee_name, task_title, completed_status])
+            csv_writer.writerow([employee_id, employee_name, completed_status, task_title])
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 1-export_to_CSV.py <employee_id>")
+        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
 
     employee_id = sys.argv[1]
