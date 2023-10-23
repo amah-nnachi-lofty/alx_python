7fb9635443e2880474717f6b65efd8611b40bb0b
amah-nnachi-lofty <loftdot@gmail.com>
2023-10-23 13:30:19 -0700

    Export to JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,99 +1,69 @@
+#!/usr/bin/python3
 """
-A Python 3 script that uses the requests module to gather information about an employee's
-TODO list progress, given the employee ID.
+2-export_to_JSON.py Script to export data from an API about an employee's TODO list progress in JSON format.
 
-This script is modified to export the data to a JSON file in the following format:
+This script uses the requests module to make requests to the API endpoints for
+getting employee details and TODO list items. It then calculates the employee's
+TODO list progress and exports it to a JSON file in the specified format.
 
-{ "USER_ID": [{"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, {"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, ... ]}
+Format must be: 
+{ "USER_ID": [
+    {"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, 
+    {"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, 
+    ... 
+]}
 
-The file name will be `USER_ID.json`.
+Args:
+  employee_id: The integer employee ID.
 
-Usage:
-
-    python 2-export_to_JSON.py EMPLOYEE_ID
+Returns:
+  None
 """
 
 import json
 import requests
+import sys
 
+# Check if the correct number of command line arguments is provided
+if len(sys.argv) != 2:
+    print("Usage: python3 2-export_to_JSON.py <employee_id>")
+    sys.exit(1)
+    
+# Extract employee ID from command line arguments
+employee_id = sys.argv[1]
 
-def get_employee_todo_items(employee_id: int) -> list:
-    """
-    Gets the TODO list items for an employee.
+# Make a request to the API endpoint to get employee details
+url_user = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+response_user = requests.get(url_user)
+user_data = response_user.json()
 
-    Args:
-        employee_id: The employee ID.
+# Check if the employee with the given ID exists
+if not user_data:
+    print(f"No employee found with ID {employee_id}")
+    sys.exit(1)
 
-    Returns:
-        A list of dictionaries, where each dictionary represents a TODO list item.
-    """
+# Make a request to the API endpoint to get employee's TODO list
+url_todos = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+response_todos = requests.get(url_todos)
+todos = response_todos.json()
 
-    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-    response = requests.get(url)
-    response.raise_for_status()
-    todo_items = response.json()
-    return todo_items
-
-
-def get_employee_details(employee_id: int) -> dict:
-    """
-    Gets the employee details for an employee.
-
-    Args:
-        employee_id: The employee ID.
-
-    Returns:
-        A dictionary representing the employee details.
-    """
-
-    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    response = requests.get(url)
-    response.raise_for_status()
-    employee_details = response.json()
-    return employee_details
-
-
-def export_employee_todo_list_to_json(employee_id: int, filename: str):
-    """
-    Exports the employee's TODO list to a JSON file.
-
-    Args:
-        employee_id: The employee ID.
-        filename: The name of the JSON file.
-    """
-
-    todo_items = get_employee_todo_items(employee_id)
-    employee_details = get_employee_details(employee_id)
-
-    json_data = {
-        "USER_ID": employee_id,
-        "tasks": [
-            {
-                "task": todo_item["title"],
-                "completed": todo_item["completed"],
-                "username": employee_details["name"],
-            }
-            for todo_item in todo_items
-        ],
+# Create a list of TODO items with the specified format
+todo_list = []
+for todo in todos:
+    task_data = {
+        "task": todo.get("title"),
+        "completed": todo.get("completed"),
+        "username": user_data.get("username")
     }
+    todo_list.append(task_data)
 
-    with open(filename, "w") as f:
-        json.dump(json_data, f, indent=4)
+# Create the output dictionary with user ID as the key and TODO list as the value
+output_data = {employee_id: todo_list}
 
+# Export data to JSON file
+output_file_name = f"{employee_id}.json"
+with open(output_file_name, "w") as json_file:
+    json.dump(output_data, json_file, indent=4)
 
-def main():
-    """
-    The main function.
-    """
-
-    employee_id = int(input("Enter the employee ID: "))
-
-    filename = f"{employee_id}.json"
-    export_employee_todo_list_to_json(employee_id, filename)
-
-    print(f"Successfully exported employee TODO list to JSON file: {filename}")
-
-
-if __name__ == "__main__":
-    main()
+print(f"Data exported to {output_file_name} successfully.")
 
