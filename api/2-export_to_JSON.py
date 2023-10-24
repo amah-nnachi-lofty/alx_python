e9011e3ded5160a05ba316c9b97acc47df06e66f
amah-nnachi-lofty <loftdot@gmail.com>
2023-10-23 21:42:48 -0700

    export to json

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 2-export_to_JSON.py Script to export data from an API about an employee's TODO list progress in JSON format.
 
@@ -26,33 +25,36 @@ import requests
 import sys
 
 # Function to export TODO list data to a JSON file
-def export_to_CSV(user_id):
+def export_to_JSON(user_id):
     # Make a request to get employee's name from the API
     employee_name = requests.get(
         "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
     ).json()["username"]
+    
     # Make a request to get employee's TODO list from the API
     tasks = requests.get(
         "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id)
     ).json()
 
-    # Prepare the data in the specified format
-    tasks_data = {str(user_id): []}
-
+    # Prepare the data in the specified format as a list of dictionaries
+    tasks_data = []
     for task in tasks:
-        tasks_data[str(user_id)].append(
-            {
-                "task": task["title"],
-                "completed": task["completed"],
-                "username": employee_name,
-            }
-        )
+        task_dict = {
+            "task": task["title"],
+            "completed": task["completed"],
+            "username": employee_name
+        }
+        tasks_data.append(task_dict)
+
+    # Create a dictionary with USER_ID as key and list of dictionaries as value
+    output_data = {str(user_id): tasks_data}
 
     # Write the data to a JSON file with USER_ID.json as the filename
     with open(str(user_id) + ".json", "w", encoding="UTF8", newline="") as f:
-        json.dump(tasks_data, f)
+        json.dump(output_data, f, indent=4)
 
 # Entry point of the script
 if __name__ == "__main__":
     # Get the employee ID from the command-line argument and call the export function
-    export_to_CSV(sys.argv[1])
+    export_to_JSON(sys.argv[1])
+
